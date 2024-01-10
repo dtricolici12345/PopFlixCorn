f8c6cf64a2f0dbb3e37b76330273b284e0e134fd
40Sabrina <sabrinaaaaa@hotmail.com>
2024-01-10 16:54:49 +0100

    first commit, carte acteur avec photo, nom acteur et nom personnage récupérés

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,10 @@
 import "./App.css";
+import CarteActeur from "./Components/CarteActeur";
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <CarteActeur />
     </div>
   );
 }
---
+++ frontend/src/Components/CarteActeur.jsx
@@ -0,0 +1,49 @@
+import { useState, useEffect } from "react";
+import "./CarteActeur.css";
+
+function CarteActeur() {
+  const [acteursInfos, setActeursInfos] = useState([]);
+  // const array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+  // const slicedArray = array.slice(0, 10);
+  // console.log(slicedArray);
+
+  useEffect(() => {
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxZTJhZGFmMTkyNDExODRhZGFhMWI1MzZhZWIwMGRiOCIsInN1YiI6IjY1OTY2OWNkNjBjNTFkM2U5ODk3ODQ4YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.bvj9pVHbgf2EV009bseL1_lV-oBEY2fUQqzOUqNXaXs",
+      },
+    };
+    fetch(
+      "https://api.themoviedb.org/3/movie/550/credits?language=en-US",
+      options
+    )
+      .then((res) => res.json())
+      .then((data) => setActeursInfos(data.cast));
+  }, []);
+
+  return (
+    <div>
+      {console.log("je suis dans le return", acteursInfos)};
+      {acteursInfos.map((acteurInfo) => (
+        <div className="container_card" key={acteurInfo.id}>
+          <li className="card">
+            {/* <a href="profile-acteur"> */}
+            <img
+              className="profile_path"
+              src={`https://image.tmdb.org/t/p/w500/${acteurInfo.profile_path}`}
+              alt="Picture_acteur"
+            />
+            {/* </a> */}
+            <p className="original_name">{acteurInfo.original_name}</p>
+            <p className="character">{acteurInfo.character}</p>
+          </li>
+        </div>)
+      )};
+    </div>
+  );
+}
+
+export default CarteActeur;
