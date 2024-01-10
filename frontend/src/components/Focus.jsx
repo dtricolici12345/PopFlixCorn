41799477e8f325518e7cb93f21d3b838aea694c0
Paul-Lac <plnlacroix@gmail.com>
2024-01-10 18:00:46 +0100

    Improve design

--- frontend/src/components/Focus.jsx
+++ frontend/src/components/Focus.jsx
@@ -10,20 +10,16 @@ function Focus({ movie }) {
 
   const [movieDetail, setMovieDetail] = useState([]);
 
-  const getDetail = () => {
+  useEffect(() => {
     fetch(
       `https://api.themoviedb.org/3/movie/${movie.id}?api_key=1e2adaf19241184adaa1b536aeb00db8&language=fr-FR&region=FR`
     )
       .then((response) => response.json())
-      .then((json) => setMovieDetail(json));
-    // .catch((err) => console.log(err));
-  };
-
-  useEffect(() => {
-    getDetail();
+      .then((data) => setMovieDetail(data))
+      .catch((err) => console.info(err));
   }, [movie.id]);
 
-  // console.log(movieDetail);
+  console.info(movieDetail);
 
   const toHoursAndMinutes = (totalMinutes) => {
     const hours = Math.floor(totalMinutes / 60);
@@ -33,37 +29,32 @@ function Focus({ movie }) {
 
   return (
     <div className="mfocus-card">
-      <div className="mfocus-card-image">
+      <div className="mfocus-card-bloc-image">
         <img
           src={`https://image.tmdb.org/t/p/w500${movieDetail.poster_path}`}
           alt={movieDetail.title}
         />
-        <div>{Math.round(movieDetail.vote_average * 10)}%</div>
+        <div className="mfocus-note">
+          {Math.round(movieDetail.vote_average * 10)}%
+        </div>
       </div>
       <div className="mfocus-card-text">
         <div className="mfocus-firstline">
           <span className="mfocus-title">{movieDetail.title} </span>
           <span className="mfocus-year-time">
-            (
-            {movieDetail.release_date
-              ? movieDetail.release_date.slice(0, 4)
-              : "Unknown"}{" "}
-            - {toHoursAndMinutes(movieDetail.runtime)})
+            ({movieDetail.release_date?.slice(0, 4)} -
+            {toHoursAndMinutes(movieDetail.runtime)})
           </span>
         </div>
         <div className="mfocus-secondline">
           <span className="mfocus-certification">age</span>
-          {movieDetail.genres && movieDetail.genres.length > 0 && (
-            <span className="mfocus-genre">
-              {movieDetail.genres.map((genre, index) =>
-                index === movieDetail.genres.length - 1 ? (
-                  <span>{genre.name}</span>
-                ) : (
-                  <span>{genre.name}, </span>
-                )
-              )}
-            </span>
-          )}
+          <span className="mfocus-genre">
+            {movieDetail.genres?.map((genre, index) =>
+              index === movieDetail.genres.length - 1
+                ? `${genre.name}`
+                : `${genre.name}, `
+            )}
+          </span>
         </div>
         <div className="mfocus-tagline">{movieDetail.tagline}</div>
         <div className="mfocus-overview">{movieDetail.overview}</div>
