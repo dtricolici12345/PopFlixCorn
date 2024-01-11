22e45ab73fdda337c5cc3474e6cb2b3d9a353075
Paul-Lac <plnlacroix@gmail.com>
2024-01-11 15:20:06 +0100

    Add certifications for movies and all info for tv shows

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,45 @@ import Focus from "./components/Focus";
 import "./App.css";
 
 function App() {
-  const movie = { id: 157336 };
+  // Oppenheimer
+  const video1 = {
+    media_type: "movie",
+    id: 872585,
+  };
+
+  // Asterix & Obelix
+  // const video2 = {
+  //   media_type: "movie",
+  //   id: 643215,
+  // };
+
+  // Napoléon
+  // const video3 = {
+  //   media_type: "movie",
+  //   id: 753342,
+  // };
+
+  // Friends
+  // const video4 = {
+  //   media_type: "tv",
+  //   id: 1668,
+  // };
+
+  // Breaking bad
+  // const video5 = {
+  //   media_type: "tv",
+  //   id: 1396,
+  // };
+
+  // The Handmaid's Tale
+  // const video6 = {
+  //   media_type: "tv",
+  //   id: 69478,
+  // };
+
   return (
     <div className="App">
-      <Focus movie={movie} />
+      <Focus video={video1} />
     </div>
   );
 }
--- frontend/src/components/Focus.jsx
+++ frontend/src/components/Focus.jsx
@@ -2,62 +2,138 @@ import { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import "../styles/Focus.css";
 
-function Focus({ movie }) {
+function Focus({ video }) {
   Focus.propTypes = {
-    movie: PropTypes.string.isRequired,
+    video: PropTypes.string.isRequired,
     id: PropTypes.number.isRequired,
+    media_type: PropTypes.string.isRequired,
   };
 
   const [movieDetail, setMovieDetail] = useState([]);
+  const [movieCertification, setMovieCertification] = useState([]);
+  const [tvDetail, setTvDetail] = useState([]);
+  const [tvCertification, setTvCertification] = useState([]);
 
   useEffect(() => {
     fetch(
-      `https://api.themoviedb.org/3/movie/${movie.id}?api_key=1e2adaf19241184adaa1b536aeb00db8&language=fr-FR&region=FR`
+      `https://api.themoviedb.org/3/movie/${video.id}?api_key=1e2adaf19241184adaa1b536aeb00db8&language=fr-FR&region=FR`
     )
       .then((response) => response.json())
       .then((data) => setMovieDetail(data))
       .catch((err) => console.info(err));
-  }, [movie.id]);
+    fetch(
+      `https://api.themoviedb.org/3/movie/${video.id}/release_dates?api_key=1e2adaf19241184adaa1b536aeb00db8&language=fr-FR&region=FR`
+    )
+      .then((response) => response.json())
+      .then((data) => setMovieCertification(data.results))
+      .catch((err) => console.info(err));
+    fetch(
+      `https://api.themoviedb.org/3/tv/${video.id}?api_key=1e2adaf19241184adaa1b536aeb00db8&language=fr-FR&region=FR`
+    )
+      .then((response) => response.json())
+      .then((data) => setTvDetail(data))
+      .catch((err) => console.info(err));
+    fetch(
+      `https://api.themoviedb.org/3/tv/${video.id}/content_ratings?api_key=1e2adaf19241184adaa1b536aeb00db8&language=fr-FR&region=FR`
+    )
+      .then((response) => response.json())
+      .then((data) => setTvCertification(data.results))
+      .catch((err) => console.info(err));
+  }, [video.id]);
 
   console.info(movieDetail);
+  // console.info(movieCertification);
+  console.info(tvDetail);
+  console.info(tvCertification);
+
+  const getBorderColor = () => {
+    const note =
+      video.media_type === "movie"
+        ? Math.round(movieDetail.vote_average * 10)
+        : Math.round(tvDetail.vote_average * 10);
+    if (note < 50) return "border-red";
+    if (note < 70) return "border-orange";
+    return "border-green";
+  };
 
   const toHoursAndMinutes = (totalMinutes) => {
     const hours = Math.floor(totalMinutes / 60);
-    const minutes = (totalMinutes - hours) % 60;
+    const minutes = Math.floor((totalMinutes - hours) / 60);
     return `${hours}h${minutes}m`;
   };
 
   return (
+    // <>
+    //   {media.media_type === "movie" ? (
     <div className="mfocus-card">
       <div className="mfocus-card-bloc-image">
         <img
-          src={`https://image.tmdb.org/t/p/w500${movieDetail.poster_path}`}
-          alt={movieDetail.title}
+          src={
+            video.media_type === "movie"
+              ? `https://image.tmdb.org/t/p/w500${movieDetail.poster_path}`
+              : `https://image.tmdb.org/t/p/w500${tvDetail.poster_path}`
+          }
+          alt={video.media_type === "movie" ? movieDetail.title : tvDetail.name}
         />
-        <div className="mfocus-note">
-          {Math.round(movieDetail.vote_average * 10)}%
+        <div className={`mfocus-note ${getBorderColor()}`}>
+          {video.media_type === "movie"
+            ? Math.round(movieDetail.vote_average * 10)
+            : Math.round(tvDetail.vote_average * 10)}
+          %
         </div>
       </div>
       <div className="mfocus-card-text">
         <div className="mfocus-firstline">
-          <span className="mfocus-title">{movieDetail.title} </span>
-          <span className="mfocus-year-time">
-            ({movieDetail.release_date?.slice(0, 4)} -
-            {toHoursAndMinutes(movieDetail.runtime)})
+          <span className="mfocus-title">
+            {video.media_type === "movie" ? movieDetail.title : tvDetail.name}
+          </span>
+          <span className="mfocus-year">
+            (
+            {video.media_type === "movie"
+              ? movieDetail.release_date?.slice(0, 4)
+              : tvDetail.first_air_date?.slice(0, 4)}
+            )
           </span>
         </div>
         <div className="mfocus-secondline">
-          <span className="mfocus-certification">age</span>
+          <span className="mfocus-certification">
+            {video.media_type === "movie"
+              ? movieCertification.find(
+                  (certification) => certification.iso_3166_1 === "FR"
+                )?.release_dates[0]?.certification
+              : tvCertification.find((rating) => rating.iso_3166_1 === "FR")
+                  ?.rating}
+          </span>
           <span className="mfocus-genre">
-            {movieDetail.genres?.map((genre, index) =>
-              index === movieDetail.genres.length - 1
-                ? `${genre.name}`
-                : `${genre.name}, `
-            )}
+            {video.media_type === "movie"
+              ? movieDetail.genres?.map((genre, index) =>
+                  index === movieDetail.genres.length - 1
+                    ? `${genre.name}`
+                    : `${genre.name}, `
+                )
+              : tvDetail.genres?.map((genre, index) =>
+                  index === tvDetail.genres.length - 1
+                    ? `${genre.name}`
+                    : `${genre.name}, `
+                )}
+          </span>
+          <img src="../assets/time-and-date.png" alt="logo time" />
+          <span className="mfocus-time">
+            {video.media_type === "movie"
+              ? toHoursAndMinutes(movieDetail.runtime)
+              : `${tvDetail.number_of_seasons} saisons`}
           </span>
         </div>
-        <div className="mfocus-tagline">{movieDetail.tagline}</div>
-        <div className="mfocus-overview">{movieDetail.overview}</div>
+        <div className="mfocus-tagline">
+          {video.media_type === "movie"
+            ? movieDetail.tagline
+            : tvDetail.tagline}
+        </div>
+        <div className="mfocus-overview">
+          {video.media_type === "movie"
+            ? movieDetail.overview
+            : tvDetail.overview}
+        </div>
         <div>
           <button type="button" className="mfocus-button">
             A voir
