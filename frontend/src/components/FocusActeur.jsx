0f8184cf88ac27aea74b5ad44be6f0b5e13ca572
cedric chemin <cedric.chemin33@gmail.com>
2024-01-11 15:04:12 +0100

    finalisation partie 1 avec css

---
+++ frontend/src/components/FocusActeur.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "../styles/FocusActeur.css";
+
+function FocusActeur({ user }) {
+  if (!user) {
+    return null;
+  }
+  return (
+    user && (
+      <div className="FocusActeur">
+        <img
+          src={`https://image.tmdb.org/t/p/w500/${user.profile_path}`}
+          alt={user.name}
+        />
+        <div className="partie">
+          <div className="partie1">
+            <h1>{user.name}</h1>
+            <p className="age">({user.birthday})</p>
+          </div>
+          <br />
+          <h2>Biographie</h2>
+          <br />
+          <p className="biography">{user.biography}</p>
+        </div>
+        <div className="partie film">
+          <div className="card1">
+            {/* <img src={`https://image.tmdb.org/t/p/w500${movieDetail.poster_path}`}
+                            alt={movieDetail.title}/> */}
+          </div>
+        </div>
+      </div>
+    )
+  );
+}
+
+FocusActeur.propTypes = {
+  user: PropTypes.shape({
+    profile_path: PropTypes.string,
+    name: PropTypes.string,
+    birthday: PropTypes.string,
+    biography: PropTypes.string,
+  }),
+};
+
+FocusActeur.defaultProps = {
+  user: null,
+};
+
+export default FocusActeur;
