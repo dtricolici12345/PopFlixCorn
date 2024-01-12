7673c3dede10198f247bbdd9859bad5136b3e8ea
Paul-Lac <plnlacroix@gmail.com>
2024-01-12 16:52:09 +0100

    First merge

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,38 +3,14 @@ import "./App.css";
 
 function App() {
   // Oppenheimer
-  // const video1 = {
-  //   media_type: "movie",
-  //   id: 872585,
-  // };
-
-  // Asterix & Obelix
-  // const video2 = {
-  //   media_type: "movie",
-  //   id: 643215,
-  // };
-
-  // Friends
-  // const video3 = {
-  //   media_type: "tv",
-  //   id: 1668,
-  // };
-
-  // Breaking bad
-  // const video4 = {
-  //   media_type: "tv",
-  //   id: 1396,
-  // };
-
-  // The Handmaid's Tale
-  const video5 = {
-    media_type: "tv",
-    id: 69478,
+  const video1 = {
+    media_type: "movie",
+    id: 181808,
   };
 
   return (
     <div className="App">
-      <Focus video={video5} />
+      <Focus video={video1} />
     </div>
   );
 }
